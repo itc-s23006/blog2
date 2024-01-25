f4509d3d546d6a637158147f9c5b5adc2ba24413
s23006 <s23006@std.it-college.ac.jp>
2024-01-25 05:20:01 +0000

    ヘッダーとフッターの横幅を整える。 Containerコンポーネントをインポート、 中身を<Container>~</Container>で囲む。

--- components/header.js
+++ components/header.js
@@ -1,14 +1,17 @@
 import Logo from 'components/logo'
 import Nav from 'components/nav'
 import styles from 'styles/header.module.css'
+import Container from 'components/container'
 
 const Header = () => {
   return (
     <header>
-      <div className={styles.flexContainer}>
-        <Logo boxOn />
-        <Nav />
-      </div>
+      <Container>
+        <div className={styles.flexContainer}>
+          <Logo boxOn />
+          <Nav />
+        </div>
+      </Container>
     </header>
   )
 }
