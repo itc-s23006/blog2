08b147e883eb609ec36e59262572ce982d13f35c
s23006 <s23006@std.it-college.ac.jp>
2024-01-25 05:53:22 +0000

    メインコンテンツの横幅を整える。 Containerコンポーネントをインポート、 コンテンツ全体を<Container>で囲む。

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,7 +1,12 @@
 import Hero from 'components/hero'
+import Container from 'components/container'
 
 const Blog = () => {
-  return <Hero title='Blog' subtitle='Recent Posts' />
+  return (
+    <Container>
+      <Hero title='Blog' subtitle='Recent Posts' />
+    </Container>
+  )
 }
 
 export default Blog
