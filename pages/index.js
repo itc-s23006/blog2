42518d2e3b87ac217b0d8913684994ce42503010
s23006 <s23006@std.it-college.ac.jp>
2024-01-30 07:46:29 +0000

    Metaコンポーネントを追加。 トップページではpageTitle属性をしない。

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Hero from 'components/hero'
 import Container from 'components/container'
+import Meta from 'components/meta'
 
 const Home = () => {
   return (
     <Container>
+    <Meta />
       <Hero title='CUBE' subtitle='アウトプットしていくサイト' imageOn />
     </Container>
   )
