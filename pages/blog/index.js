6269435e1c4015c2a7ca750cda2a96cd2e408cb0
s23006 <s23006@std.it-college.ac.jp>
2024-01-30 07:48:52 +0000

    Metaコンポーネントを追加。

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,9 +1,11 @@
 import Hero from 'components/hero'
 import Container from 'components/container'
+import Meta from 'components/meta'
 
 const Blog = () => {
   return (
     <Container>
+      <Meta pageTitle='ブログ' />
       <Hero title='Blog' subtitle='Recent Posts' />
     </Container>
   )
