32b89f86c30f58d8dad6120994d13bd469dabe98
s23006 <s23006@std.it-college.ac.jp>
2024-01-30 07:27:20 +0000

    siteMetaをインポート、分割代入で使いたい情報を取り出します。 ここでは<head>内に入れる設定に必要なもの(siteLang以外)を 取り出しています。

--- components/meta.js
+++ components/meta.js
@@ -1,5 +1,16 @@
 import Head from 'next/head'
 
+/* サイトに関する情報 */
+import { siteMeta } from 'lib/constants'
+const {
+  siteTitle,
+  siteDesc,
+  siteUrl,
+  siteLocale,
+  siteType,
+  siteIcon
+} = siteMeta
+
 const Meta = ({ pageTitle }) => {
   return (
     <Head>
