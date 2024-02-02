9f7285b852b86a63658a16a65ee4ea38295ab273
s23006 <s23006@std.it-college.ac.jp>
2024-02-02 04:06:30 +0000

    *カテゴリーをリストにする。

---
+++ components/post-categories.js
@@ -0,0 +1,18 @@
+import styles from 'styles/post-categories.module.css'
+import Link from 'next/link'
+
+const PostCategories = ({ categories }) => {
+return (
+<ul className={styles.list}>
+  {categories.map(({ name, slug }) => (
+  <li key={slug}
+  <Link href={`/blog/category/${slug}`}>
+  <a>{name}</a>
+  </Link>
+  </li>
+  ))}
+</ul>
+)
+}
+
+export default PostCategories
