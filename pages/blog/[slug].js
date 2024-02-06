944bb95c3fc79df9adcc52720ec711f4f9dd13c4
s23006 <s23006@std.it-college.ac.jp>
2024-02-06 07:05:23 +0000

    *[slug].jsを用意する。 schedule.jsのファイル名を[slug].jsに変更。 コンポーネント名も「Schedule」から「Post」に変更。

---
+++ pages/blog/[slug].js
@@ -0,0 +1,93 @@
+import { getPostBySlug } from 'lib/api'
+import Container from 'components/container'
+import PostHeader from 'components/post-header'
+import Image from 'next/legacy/image'
+import {
+  TwoColumn,
+  TwoColumnMain,
+  TwoColumnSidebar
+} from 'components/two-column'
+import PostBody from 'components/post-body'
+import ConvertBody from 'components/convert-body'
+import PostCategories from 'components/post-categories'
+import { extractText } from 'lib/extract-text'
+import Meta from 'components/meta'
+import { eyecatchLocal } from 'lib/constants'
+import { getPlaiceholder } from 'plaiceholder'
+
+const Post = ({
+  title,
+  publish,
+  content,
+  eyecatch,
+  categories,
+  description
+}) => {
+  return (
+    <Container>
+      <Meta
+        pageTitle={title}
+        pageDesc={description}
+        pageImg={eyecatch.url}
+        pageImgW={eyecatch.width}
+        pageImgH={eyecatch.height}
+      />
+
+      <article>
+        <PostHeader title={title} subtitle='Blog Article' publish={publish} />
+
+        <figure>
+          <Image
+            src={eyecatch.url}
+            alt=''
+            layout='responsive'
+            width={eyecatch.width}
+            height={eyecatch.height}
+            sizes='(min-width: 1152px) 1152px, 100vw'
+            priority
+            plaiceholder='blur'
+            blurDataURL={eyecatch.blurDataURL}
+          />
+        </figure>
+
+        <TwoColumn>
+          <TwoColumnMain>
+            <PostBody>
+              <ConvertBody contentHTML={content} />
+            </PostBody>
+          </TwoColumnMain>
+          <TwoColumnSidebar>
+            <PostCategories categories={categories} />
+          </TwoColumnSidebar>
+        </TwoColumn>
+      </article>
+    </Container>
+  )
+}
+
+const getStaticProps = async () => {
+  const slug = 'micro'
+
+  const post = await getPostBySlug(slug)
+
+  const description = extractText(post.content)
+
+  const eyecatch = post.eyecatch ?? eyecatchLocal
+
+  const { base64 } = await getPlaiceholder(eyecatch.url)
+  eyecatch.blurDataURL = base64
+
+  return {
+    props: {
+      title: post.title,
+      publish: post.publishDate,
+      content: post.content,
+      eyecatch,
+      categories: post.categories,
+      description
+    }
+  }
+}
+
+export default Post
+export { getStaticProps }
