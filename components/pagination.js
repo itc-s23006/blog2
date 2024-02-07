a5b852b6720aab4494c02936f0906b638332d782
s23006 <s23006@std.it-college.ac.jp>
2024-02-07 08:09:10 +0000

    *ページネーションのコンポーネントを作成。

---
+++ components/pagination.js
@@ -0,0 +1,41 @@
+import styles from 'styles/pagination.module.css'
+import Link from 'next/link'
+import { FontAwesomeicon } from '@fortawesome/react-awesome'
+import {
+  faChevronLeft,
+  faChevronRight
+} from '@fortawesome/free-solid-svg-icons'
+
+const Pagination = ({
+  prevText = '',
+  prevUrl = '',
+  nextText = '',
+  nextUrl = ''
+}) => {
+  return (
+    <ul className={styles.flexContainer}>
+      {prevText && prevUrl && (
+        <li className={styles.prev}>
+          <Link href={prevUrl}>
+            <a className={styles.iconText}>
+              <FontAwesomeIcon icon={faChevronLeft} color='var(--gray-25)' />
+              <span>{prevText}</span>
+            </a>
+          </Link>
+        </li>
+      )}
+      {nextText && nextUrl && (
+        <li className={styles.next}>
+          <Link href={nextUrl}>
+            <a className={styles.iconText}>
+              <span>{nextText}</span>
+              <FontAwesomeIcon icon={faChevronRight} color='var(--gray-25)' />
+            </a>
+          </Link>
+        </li>
+      )}
+    </ul>
+  )
+}
+
+export default Pagination
