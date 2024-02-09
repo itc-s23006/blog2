5f63802dee79882f6bd97162c332b1963c3d7eb2
s23006 <s23006@std.it-college.ac.jp>
2024-02-09 02:34:40 +0000

    *代替画像とブラー画像を追加。* 代替画像のデータeyecatchLocalと、 ブラー画像を生成するgetPlaiceholder()関数をインポート。

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -3,6 +3,8 @@ import Container from 'components/container'
 import Meta from 'components/meta'
 import { getAllPosts } from 'lib/api'
 import Posts from 'components/posts'
+import { getPlaiceholder } from 'plaiceholder'
+import { eyecatchLocal } from 'lib/constants'
 
 const Blog = ({ posts }) => {
   return (
@@ -17,6 +19,14 @@ const Blog = ({ posts }) => {
 
 const getStaticProps = async () => {
   const posts = await getAllPosts()
+
+  for (const post of posts) {
+    if (!post.hasOwnProperty('eyecatch')) {
+      post.eyecatch = eyecatchLocal
+    }
+    const { base64 } = await getPlaiceholder(post.eyecatch.url)
+    post.eyecatch.blurDataURL = base64
+  }
 
   return {
     props: {
