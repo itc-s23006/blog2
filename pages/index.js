260e3764a32843609dc35f47764d394db4c2878e
s23006 <s23006@std.it-college.ac.jp>
2024-02-09 11:12:50 +0000

    "修正"

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,43 @@
 import Hero from 'components/hero'
 import Container from 'components/container'
 import Meta from 'components/meta'
+import { getAllPosts } from 'lib/api'
+import Posts from 'components/posts'
+import Pagination from 'components/pagination'
+import { getPlaiceholder } from 'plaiceholder'
+import { eyecatchLocal } from 'lib/constants'
+import { getImageBuffer } from 'lib/getImageBuffer'
 
-const Home = () => {
+const Home = ({ posts }) => {
   return (
     <Container>
-    <Meta />
+      <Meta />
       <Hero title='CUBE' subtitle='アウトプットしていくサイト' imageOn />
+
+      <Posts posts={posts} />
+      <Pagination nextUrl='/blog' nextText='More Posts' />
     </Container>
   )
 }
 
+const getStaticProps = async () => {
+  const posts = await getAllPosts(4)
+
+  for (const post of posts) {
+    if (!post.hasOwnProperty('eyecatch')) {
+      post.eyecatch = eyecatchLocal
+    }
+    const imageBuffer = await getImageBuffer(post.eyecatch.url)
+    const { base64 } = await getPlaiceholder(imageBuffer)
+    post.eyecatch.blurDataURL = base64
+  }
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
+
 export default Home
+export { getStaticProps }
