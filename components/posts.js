84580f17257097330bc0accde2ef6d5722fd14c6
s23006 <s23006@std.it-college.ac.jp>
2024-02-09 02:38:54 +0000

    *記事一覧のレイアウトを整える。* 記事一覧は2列のタイル状に並べたレイアウトにします。

--- components/posts.js
+++ components/posts.js
@@ -16,6 +16,7 @@ const Posts = ({ posts }) => {
                   layout='responsive'
                   width={eyecatch.width}
                   height={eyecatch.height}
+                  sizes='(min-width: 1152px) 576px, 50vw'
                   placeholder='blur'
                   blurDataURL={eyecatch.blurDataURL}
                 />
