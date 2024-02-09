be3241a89e5a8c4c87eb5d8898ae53ba56c55109
s23006 <s23006@std.it-college.ac.jp>
2024-02-09 02:23:36 +0000

    記事一覧ページblog/index.jsに Postsコンポーネントをインポート。

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -2,12 +2,15 @@ import Hero from 'components/hero'
 import Container from 'components/container'
 import Meta from 'components/meta'
 import { getAllPosts } from 'lib/api'
+import Posts from 'components/posts'
 
 const Blog = ({ posts }) => {
   return (
     <Container>
       <Meta pageTitle='ブログ' pageDesc='ブログの記事一覧' />
       <Hero title='Blog' subtitle='Recent Posts' />
+
+      <Posts posts={posts} />
     </Container>
   )
 }
